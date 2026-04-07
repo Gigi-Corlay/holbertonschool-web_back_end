7a564a5fee7025e266360697bcac498398c61b5d
Gigi-Corlay <11489@holbertonstudents.com>
2026-04-07 14:44:05 +0200

    Add type-annotated safe_first_element using Sequence, Any and Optional

---
+++ python_variable_annotations/100-safe_first_element.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+from typing import Sequence, Any, Optional
+
+
+def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
+    """
+    Return the first element of a sequence or None.
+    """
+    if lst:
+        return lst[0]
+    else:
+        return None
