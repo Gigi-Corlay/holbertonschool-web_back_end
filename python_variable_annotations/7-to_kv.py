78000be78647616d16d04e279a66803a4c7c4303
Gigi-Corlay <11489@holbertonstudents.com>
2026-04-08 11:23:59 +0200

    2nd correction task 7

--- python_variable_annotations/7-to_kv.py
+++ python_variable_annotations/7-to_kv.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """
-Module that provides a function to create a key-value pair with a squared value.
+Module that provides a function to create
+a key-value pair with a squared value.
 """
 
 from typing import Union, Tuple
@@ -8,12 +9,6 @@ from typing import Union, Tuple
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
     """
-    Create a tuple with a string and the square of a number.
-
-    Args:
-        k (str): The key of the tuple.
-        v (Union[int, float]): The number to square.
-
     Returns:
         Tuple[str, float]: A tuple containing k and the square of v as a float.
     """
