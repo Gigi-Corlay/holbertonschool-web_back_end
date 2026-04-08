722041af54c1844749fe1f60fa2507e3f6578551
Gigi-Corlay <11489@holbertonstudents.com>
2026-04-08 12:40:22 +0200

    Correction comment 100

--- python_variable_annotations/100-safe_first_element.py
+++ python_variable_annotations/100-safe_first_element.py
@@ -1,11 +1,11 @@
 #!/usr/bin/env python3
-
 from typing import Sequence, Any, Optional
 
 
 def safe_first_element(lst: Sequence[Any]) -> Optional[Any]:
     """
     Return the first element of a sequence or None.
+    If the sequence is empty, returns None.
     """
     if lst:
         return lst[0]
