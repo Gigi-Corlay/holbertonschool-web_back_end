31f29fff40c0bfc4fa4b5d0ccf8393ce37d02f93
Gigi-Corlay <11489@holbertonstudents.com>
2026-04-08 10:59:44 +0200

    Correction comment task2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
-
+"""
+Module that provides a function to compute the floor of a float number.
+"""
 import math
 
 
 def floor(n: float) -> int:
     """
-    Return the floor of a float number.
+    Return the floor of float n as an integer.
     """
     return math.floor(n)
