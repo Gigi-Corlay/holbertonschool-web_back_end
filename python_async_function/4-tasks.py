c98f2242e8a4f4dbafd9c53b4555bb61f39229bb
Gigi-Corlay <11489@holbertonstudents.com>
2026-04-09 11:31:29 +0200

    Return results in ascending order of completion

--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -9,6 +9,7 @@ from typing import List
 # Import task_wait_random depuis 3-tasks.py
 task_wait_random = __import__('3-tasks').task_wait_random
 
+
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
     Launch task_wait_random n times with max_delay and return list of results
