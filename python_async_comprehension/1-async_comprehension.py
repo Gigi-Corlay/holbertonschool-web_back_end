9d9e618db581ec674a80aa012fb9d6110b960059
Gigi-Corlay <11489@holbertonstudents.com>
2026-04-09 17:05:11 +0200

    Fix async generator and add async comprehension coroutine

--- python_async_comprehension/1-async_comprehension.py
+++ python_async_comprehension/1-async_comprehension.py
@@ -1,18 +1,11 @@
 #!/usr/bin/env python3
-"""
-Module that defines a coroutine using async comprehension to collect random numbers
-from an asynchronous generator.
-"""
+"""Module that defines an asynchronous comprehension coroutine."""
+
+from typing import List
 
 async_generator = __import__('0-async_generator').async_generator
-from typing import List
 
 
 async def async_comprehension() -> List[float]:
-    """
-    Collects 10 random numbers asynchronously using async comprehension.
-
-    Returns:
-        List[float]: A list containing 10 random numbers.
-    """
-    return [number async for number in async_generator()]
+    """Collect 10 random floating-point numbers from async_generator."""
+    return [i async for i in async_generator()]
