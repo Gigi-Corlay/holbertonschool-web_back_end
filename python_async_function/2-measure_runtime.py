e8bd7dae51cd326502c64cc36315a88c30b8aca0
Gigi-Corlay <11489@holbertonstudents.com>
2026-04-09 10:42:02 +0200

    Use time module to measure total execution time

---
+++ python_async_function/2-measure_runtime.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+"""
+Measure runtime for wait_n coroutine and calculate average delay
+"""
+import time
+import asyncio
+
+# Import depuis 1-concurrent_coroutines.py
+wait_n = __import__('1-concurrent_coroutines').wait_n
+
+
+def measure_time(n: int, max_delay: int) -> float:
+    """
+    Measures the total execution time of wait_n(n, max_delay)
+    and returns the average time per coroutine.
+    """
+    start_time = time.time()
+    asyncio.run(wait_n(n, max_delay))
+    end_time = time.time()
+    total_time = end_time - start_time
+    return total_time / n
