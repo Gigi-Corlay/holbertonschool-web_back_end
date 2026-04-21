61c38e61c959c229649ca87d5148fca73a427b13
Gigi-Corlay <11489@holbertonstudents.com>
2026-04-21 07:37:57 +0000

    Fix get_page implementation and pagination logic

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -44,7 +44,7 @@ class Server:
         start, end = index_range(page, page_size)
         data = self.dataset()
 
-        if start >= len(dataset):
+        if start >= len(data):
             return []
 
         return data[start:end]
