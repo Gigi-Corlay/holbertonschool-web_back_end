7d46a18d6e672934da103dd162e4e344bbc5c194
Gigi-Corlay <11489@holbertonstudents.com>
2026-04-23 11:07:50 +0000

    Returns the list of school having a specific topic

---
+++ NoSQL/11-schools_by_topic.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+"""
+Module to filter schools by topic
+"""
+def schools_by_topic(mongo_collection, topic):
+    """
+    Returns the list of school having a specific topic
+    """
+    schools = mongo_collection.find( {"topics": topic})
+    return list(schools)
