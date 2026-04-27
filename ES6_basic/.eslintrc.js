de7ef872dbeeb823a8b0d6b0a670c4f2f73c9876
Gigi-Corlay <11489@holbertonstudents.com>
2026-04-27 12:11:44 +0000

    0. Const or let?

---
+++ ES6_basic/.eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+    env: {
+      es6: true,
+      node: true,
+      jest: true,
+    },
+    parserOptions: {
+      sourceType: 'module'
+    },
+    extends: 'eslint:recommended',
+    rules: {},
+  };
