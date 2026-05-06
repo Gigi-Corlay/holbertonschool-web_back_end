3833b97f80ac1eeb5f4dc85c67491c019bd507b6
Gigi-Corlay <11489@holbertonstudents.com>
2026-05-06 12:07:04 +0000

    Correction

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,5 +1,5 @@
-import signUpUser from "./4-user-promise";
-import uploadPhoto from "./5-photo-reject";
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
   return Promise.allSettled([
@@ -8,10 +8,9 @@ export default function handleProfileSignup(firstName, lastName, fileName) {
   ]).then((results) =>
     results.map((result) => ({
       status: result.status,
-      value:
-        result.status === "fulfilled"
-          ? result.value
-          : String(result.reason),
+      value: result.status === 'fulfilled'
+        ? result.value
+        : result.reason,
     }))
   );
 }
