ceba4b4fba71005b4b5a6442218a5d45f3589a7f
Gigi-Corlay <11489@holbertonstudents.com>
2026-05-13 12:11:56 +0200

    a small HTTP server using the http module

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,11 +1,26 @@
 const http = require('http');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
+  res.setHeader('Content-Type', 'text/plain');
+
+  if (req.url === '/') {
     res.end('Hello Holberton School!');
+  }
+
+  else if (req.url === '/students') {
+    const db = process.argv[2];
+
+    countStudents(db)
+      .then((result) => {
+        res.end(`This is the list of our students\n${result}`);
+      })
+      .catch(() => {
+        res.end('This is the list of our students\nCannot load the database');
+      });
+  }
 });
 
 app.listen(1245);
-
-console.log('Serveur running');
 
 module.exports = app;
